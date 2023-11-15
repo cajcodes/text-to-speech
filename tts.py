9e1ac1e6f230bc61e5109c6db8c157295cd897b8
cajcodes <129537800+cajcodes@users.noreply.github.com>
2023-11-15 10:06:35 -0600

    TTS from TXT file script

--- tts.py
+++ tts.py
@@ -3,11 +3,11 @@ from openai import OpenAI
 
 client = OpenAI()
 
-speech_file_path = Path(__file__).parent / "speech-intro-tar.mp3"
+speech_file_path = Path(__file__).parent / "speech-file-name.mp3"
 response = client.audio.speech.create(
   model="tts-1-hd",
   voice="echo",
-  input="እዚ ናብ ፎኖስከይፕ ፊብር ዕርፊት፣ እንታይ ንእምነት። እኛ እዚ ናብዚ እናደርግዎ ክንመርሕዎ በእተነፃፃፈ ናይ ዝኸበር ፎኖስከይፕ ፊብር ጌትዌይ ኣለና። ክንጠልኦምና ተዘጋጁ፣ ብብልቦክሲ ዝበሉ ዕቃታት፦ ጌትዌይ (ሞዴም)፣ ብሓይቲ ዝተገጠሙ ናይ ግንባታ ሞጣቢ ከምኡ'ውን ስክራውስ እና 2 ኣንኮራ (ኦፕሽናል)፣ ናይ መንግስቲ ሰማይ፣ ኤትሬንካብል፣ እና ኮኣክሽያል ካብል። ተንቀጥቅጦ፦ መጀመርያ ናይ ኦትሌት ምርመራ"
+  input="insert text here..."
 )
 
 response.stream_to_file(speech_file_path)
---
+++ tts_txt.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from openai import OpenAI
+
+client = OpenAI()
+
+# Path to the text file and the output speech file
+text_file_path = Path(__file__).parent / "input-text.txt"
+speech_file_path = Path(__file__).parent / "speech-blog-mosaic.mp3"
+
+# Read the content of the text file
+with open(text_file_path, 'r', encoding='utf-8') as file:
+    text_content = file.read()
+
+# Call the OpenAI API to create the speech file
+response = client.audio.speech.create(
+  model="tts-1-hd",
+  voice="echo",
+  input=text_content  # Pass the read content as input
+)
+
+# Stream the response to the file
+response.stream_to_file(speech_file_path)
